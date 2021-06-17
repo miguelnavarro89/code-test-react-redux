cd41b589082dc7e543c8e953dad551846021a448
Miguel Navarro <miguelnavarro.co@gmail.com>
2021-06-17 11:31:44 -0500

    Added "add new movie" button

--- src/movies/components/movies/index.js
+++ src/movies/components/movies/index.js
@@ -1,7 +1,9 @@
-import { Grid } from "@material-ui/core";
+import { Button, Grid } from "@material-ui/core";
 import { always, cond, equals, T } from "ramda";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 import { fetchAll, selectStatus } from "../../slice";
 import Aside from "./aside";
 import Main from "./main";
@@ -18,16 +20,23 @@ function Movies() {
         <Grid container>
             {cond([
                 [equals('pending'), always('Loading...')],
-                [equals('failed'), always('An rrror occurred, try later...')],
+                [equals('failed'), always('An Error occurred, try later...')],
                 [T, always(
                     <>
                         <Aside />
                         <Main />
+                        <AddLink to='/add'>
+                            <Button color='primary' role='none' tabIndex='-1'>
+                                Add new movie
+                            </Button>
+                        </AddLink>
                     </>
                 )],
             ])(status)}
         </Grid>
     );
 }
+
+const AddLink = styled(Link)``
 
 export default Movies;
