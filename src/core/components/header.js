e942678d6f00f0be51f9c5fc50894fb8f19d710c
Miguel Navarro <miguelnavarro.co@gmail.com>
2021-06-16 22:52:39 -0500

    Add nav link styles for active route

--- src/core/components/header.js
+++ src/core/components/header.js
@@ -1,7 +1,29 @@
 import { Grid, Button } from '@material-ui/core';
-import { Link } from 'react-router-dom'
+import { map } from 'ramda';
+import { NavLink } from 'react-router-dom'
 import styled from 'styled-components';
 import logo from '../../images/the-movies-db-logo.svg'
+
+const menu = [
+    {
+        label: 'Home',
+        route: '/', 
+    },
+    {
+        label: 'Add movie',
+        route: '/add', 
+    }
+]
+
+const mapWithMenuItem = map(({ label, route }) => 
+    <li key={label}>
+        <StyledLink to={route} activeClassName='active' exact>
+            <Button variant='text' color='inherit' role='none' tabIndex='-1'>
+                {label}
+            </Button>
+        </StyledLink>
+    </li>
+)
 
 export default function Header () {
     return (
@@ -9,22 +31,7 @@ export default function Header () {
             <Grid container justify='space-between' alignItems='center'>
                     <LogoWrapper><span>The Movie DB</span> <img src={logo} alt='The Movies DB logo' width='120px' /></LogoWrapper>
                     <Nav aria-label='Primary'>
-                        <ul>
-                            <li>
-                            <StyledLink to='/'>
-                                <Button variant='text' color='inherit' role='none' tabIndex='-1'>
-                                    Home
-                                </Button>
-                            </StyledLink>
-                            </li>
-                            <li>
-                            <StyledLink to='/add'>
-                                <Button variant='text' color='inherit' role='none' tabIndex='-1'>
-                                    Add movie
-                                </Button>
-                            </StyledLink>
-                            </li>
-                        </ul>
+                        <ul>{mapWithMenuItem(menu)}</ul>
                     </Nav>
             </Grid>
         </Wrapper>
@@ -46,6 +53,10 @@ const LogoWrapper = styled.h1`
     }
 `;
 
-const StyledLink = styled(Link)`
+const StyledLink = styled(NavLink)`
     text-decoration: none;
+
+    &.active {
+        color: var(--color-primary);
+    }
 `
